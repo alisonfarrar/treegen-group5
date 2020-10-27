c5e2e26863e4209a7e11b024908aeddaf5b81336
Alison Farrar <alison.e.farrar@gmail.com>
2020-10-27 12:37:14 +0000

    Issue 18: Initialises About page

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+       <header>
+           This is the About Page.
+       </header>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
