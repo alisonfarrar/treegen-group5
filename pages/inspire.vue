da62e2e7b6fbe88ffe015d56c111f25a03e031e6
Alison Farrar <alison.e.farrar@gmail.com>
2020-10-27 12:35:10 +0000

    Issue 02: Adds sliders for user input on Inspire page

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,70 @@
+
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-card class="e4">
+ <!---<v-responsive
+      :style="{ background: `rgb(${red}, ${green}, ${blue})` }"
+      height="300px"
+    ></v-responsive>-->
+
+    <v-card-text>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12">
+            <header>
+              Number of Iterations
+            </header>
+            <v-slider
+              v-model="Iterations"
+              :max="10"
+              class="align-left"
+            >
+            </v-slider>
+          </v-col>
+
+          <v-col cols="12">
+            <header>
+              Angle of Rotation (˚)
+            <v-slider
+              v-model="angle"
+              :max="180"
+              class="align-left"
+            >
+            </v-slider>
+          </v-col>
+
+          <v-col cols="12">
+            <header>
+              Length of Initial Line
+            </header>
+            <v-slider
+              v-model="length"
+              :max="10"
+              class="align-left"
+            >
+            </v-slider>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
 </template>
+
+<!---
+<script>
+  export default {
+    data () {
+      return {
+        red: 64,
+        green: 128,
+        blue: 0,
+      }
+    },
+  }
+</script>-->
+
+<style scoped>
+.e4 {
+  width: 200px;
+  margin: auto;
+}
+</style>
