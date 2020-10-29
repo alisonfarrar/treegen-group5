81a28be756904778b595fad8ca7c8d569da33a5f
Lyle Hopkins <lyle@cosmicnetworks.com>
2020-10-29 12:29:59 +0000

    #10 Fixed inspire test warnings

--- test/pages/inspire.spec.js
+++ test/pages/inspire.spec.js
@@ -4,7 +4,25 @@ import Inspire from '@/pages/Inspire.vue'
 describe('Inspire', () => {
     test('is a Vue instance', () => {
         const wrapper = mount(Inspire, {
-            stubs: ['v-layout', 'v-flex', 'v-col'],
+            stubs: [
+                'v-btn',
+                'v-row',
+                'v-layout',
+                'v-col',
+                'v-card',
+                'v-card-title',
+                'v-card-text',
+                'v-card-actions',
+                'v-spacer',
+                'v-dialog',
+                'v-divider',
+                'v-container',
+                'v-select',
+                'v-slider',
+                'v-simple-table',
+                'v-card-subtitle',
+                'v-expand-transition',
+            ],
         })
         expect(wrapper.vm).toBeTruthy()
     })
