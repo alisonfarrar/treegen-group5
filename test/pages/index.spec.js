3d79eac66bf108495a403403af1f7cca027625d1
Alison Farrar <71133540+alisonfarrar@users.noreply.github.com>
2020-10-29 22:10:52 +0000

    Revert "Issue32 layout"

---
+++ test/pages/index.spec.js
@@ -0,0 +1,23 @@
+import { mount } from '@vue/test-utils'
+import Index from '@/pages/index.vue'
+
+describe('Index', () => {
+    test('is a Vue instance', () => {
+        const wrapper = mount(Index, {
+            stubs: [
+                'logo',
+                'vuetify-logo',
+                'v-btn',
+                'v-row',
+                'v-layout',
+                'v-col',
+                'v-card',
+                'v-card-title',
+                'v-card-text',
+                'v-card-actions',
+                'v-spacer',
+            ],
+        })
+        expect(wrapper.vm).toBeTruthy()
+    })
+})
