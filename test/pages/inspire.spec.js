5037333e6f4c0a1443adb83d151f2984de2d06f7
Lyle Hopkins <lyle@cosmicnetworks.com>
2020-10-29 20:47:45 +0000

    #10 updated tests to have 100% line coverage of our methods

--- test/pages/inspire.spec.js
+++ test/pages/inspire.spec.js
@@ -25,25 +25,67 @@ describe('Inspire', () => {
     })
     test('is a Vue instance', () => {
         expect(wrapper.vm).toBeTruthy()
-        //console.log('!!!HERE!!!', wrapper.html())
     })
 
-    it('Has an about button', () => {
+    it('Has about button', () => {
         expect(wrapper.find('#btn_about').text()).toMatch('About')
     })
 
     it('Clicks about button', async () => {
         await wrapper.find('#btn_about').trigger('click')
-        //await flushPromises()
-        //console.log( wrapper.text() )
-        //expect(wrapper.text()).toMatch('About the Project')
     })
 
-    it('Clicks submit button', async () => {
-        //console.log( wrapper.find('#btn_about').trigger('click') )
-        //expect()
+    it('Has run button', () => {
+        expect(wrapper.find('#btn_run').text()).toMatch('Run')
+    })
+
+    it('LSystem function default', async () => {
+        //await wrapper.find('#btn_run').trigger('click')
+        // Call the method directly
         wrapper.vm.submit();
-        await wrapper.find('#btn_submit').trigger('click')
+        // Check the word output
+        expect(wrapper.vm.tree_word).toEqual('FFF')
+    })
+
+    it('LSystem function example 1', async () => {
+        // Change to the first example
+        wrapper.vm.tree_selected = wrapper.vm.tree_list[0]
+        wrapper.vm.tree_change();
+        expect(wrapper.vm.settings).toEqual({
+            axiom: "'F'",
+            prod_keys: ['F'],
+            prod_values: ['FF-[-F+F+F]+[+F-F-F]'],
+            prod_str: "'F -> FF-[-F+F+F]+[+F-F-F]'",
+            iterations: 4,
+            angle: 22.5,
+            line_length: 40,
+        })
+        // Make the output manageable
+        wrapper.vm.settings.iterations = 1
+        // Call the method directly
+        wrapper.vm.submit();
+        // Check the word output
+        expect(wrapper.vm.tree_word).toEqual("'FF-[-F+F+F]+[+F-F-F]'")
+    })
+
+    it('LSystem function example 2', async () => {
+        // Change to the first example
+        wrapper.vm.tree_selected = wrapper.vm.tree_list[1]
+        wrapper.vm.tree_change();
+        expect(wrapper.vm.settings).toEqual({
+            axiom: "'X'",
+            prod_keys: ['X', 'F'],
+            prod_values: ['F[+X][-X]FX', 'FF'],
+            prod_str: "'X -> F[+X][-X]FX', 'F -> FF'",
+            iterations: 7,
+            angle: 25.7,
+            line_length: 20,
+        })
+        // Make the output manageable
+        wrapper.vm.settings.iterations = 1
+        // Call the method directly
+        wrapper.vm.submit();
+        expect(wrapper.vm.tree_word).toEqual("'F[+X][-X]FX'")
     })
 
 })
